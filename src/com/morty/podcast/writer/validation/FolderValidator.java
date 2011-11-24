894dc0210c9e00b327a5569afdba189ebefcea07
Andrew Morton <andymorton@btinternet.com>
2011-11-24 16:21:11 +0000

    Add license stuff

--- src/com/morty/podcast/writer/validation/FolderValidator.java
+++ src/com/morty/podcast/writer/validation/FolderValidator.java
@@ -1,3 +1,6 @@
+// Copyright (c) 2011, Andrew Morton. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
 
 package com.morty.podcast.writer.validation;
 
